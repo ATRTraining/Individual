571aebbc9c66420571ac245e5cd6cec372c24863
ecojocaru <eug3n_cojocaru@yahoo.co.uk>
2016-05-03 12:35:46 +0200

    - delete Readme file  - add SCJP ebook  - add a demo file structure

---
+++ Ervin Hategan/Projects/SCJP/Chapter 1/src/src/ro/ausy/training/scjp/test/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package ro.ausy.training.scjp.test;
+
+/**
+ * Test class provided as an example.
+ * 
+ * @author <a href="mailto:ecojocaru@ausy-group.com">Eugen Cojocaru </a>
+ */
+public class HelloWorld {
+
+	/**
+	 * Main method
+	 * 
+	 * @param args
+	 *            program arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
